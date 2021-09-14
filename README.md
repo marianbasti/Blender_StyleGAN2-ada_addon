47a6d6416acbb6c20813c3296e6f668231eb561f
marianobasti <31198560+marianobasti@users.noreply.github.com>
2021-09-14 13:20:55 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Blender_StyleGAN2-ada_addon
+ Implementation of NVLab's StyleGAN2-ada for material creation in Blender
---
+++ StyleGAN-ada_addon.py
@@ -0,0 +1,152 @@
+bl_info = {
+    "name": "StyleGAN",
+    "blender": (3, 0, 0),
+    "category": "Object",
+}
+
+"""Generate images using pretrained network pickle."""
+
+import bpy
+from bpy.types import Panel
+from bpy.props import *
+from bpy import context, data, ops
+import os
+import re
+from typing import List, Optional
+
+import click
+import dnnlib
+import numpy as np
+import PIL.Image
+import torch
+import legacy
+
+
+def pil_to_image(pil_image, name='texture'):
+    '''
+    PIL image pixels is 2D array of byte tuple (when mode is 'RGB', 'RGBA') or byte (when mode is 'L')
+    bpy image pixels is flat array of normalized values in RGBA order
+    '''
+    # setup PIL image conversion
+    width = pil_image.width
+    height = pil_image.height
+    byte_to_normalized = 1.0 / 255.0
+    # create new image
+    bpy_image = bpy.data.images.new(name, width=width, height=height)
+    # convert Image 'L' to 'RGBA', normalize then flatten 
+    bpy_image.pixels[:] = (np.asarray(pil_image.convert('RGBA'),dtype=np.float32) * byte_to_normalized).ravel()
+
+    return bpy_image
+
+#----------------------------------------------------------------------------
+
+def num_range(s: str) -> List[int]:
+    '''Accept either a comma separated list of numbers 'a,b,c' or a range 'a-c' and return as a list of ints.'''
+
+    range_re = re.compile(r'^(\d+)-(\d+)$')
+    m = range_re.match(s)
+    if m:
+        return list(range(int(m.group(1)), int(m.group(2))+1))
+    vals = s.split(',')
+    return [int(x) for x in vals]
+
+
+#----------------------------------------------------------------------------
+def generate_images(network_pkl, seeds, truncation_psi, noise_mode):
+    print('Loading networks from "%s"...' % network_pkl)
+    device = torch.device('cuda')
+    with dnnlib.util.open_url(network_pkl) as f:
+        G = legacy.load_network_pkl(f)['G_ema'].to(device) # type: ignore
+        
+    #G = torch.load(network_pkl)
+    print('Network is dimension %0d' % G.z_dim)
+
+    if seeds is None:
+        ctx.fail('--seeds option is required when not using --projected-w')
+
+    # Labels.
+    label = torch.zeros([1, G.c_dim], device=device)
+    
+    # Generate images.
+    for seed_idx, seed in enumerate(seeds):
+        print('Generating image for seed %d (%d/%d) ...' % (seed, seed_idx, len(seeds)))
+        z = torch.from_numpy(np.random.RandomState(seed).randn(1, G.z_dim)).to(device)
+        img = G(z, label, truncation_psi=truncation_psi, noise_mode=noise_mode)
+        img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
+        im = PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB')
+        
+        mat = bpy.context.view_layer.objects.active.active_material
+        image_node = mat.node_tree.nodes["Image Texture"]
+        for img in bpy.data.images:
+            bpy.data.images.remove(img)
+        tex =  pil_to_image(im)
+        image_node.image = tex
+        
+
+#----------------------------------------------------------------------------
+
+class properties(bpy.types.PropertyGroup):
+    network : bpy.props.StringProperty(name="Network path")
+    seed : bpy.props.IntProperty(name="Seed",default = 33)
+
+
+class stylegan_OT_run(bpy.types.Operator):
+    bl_label = "Generate Image"
+    bl_idname = "stylegan.run"
+    
+    def execute(self,context):
+        props = context.scene.props
+        generate_images(props.network, [props.seed],1,'const')
+        return{'FINISHED'}
+
+
+class MyPanel(Panel):
+    bl_label = 'StyleGAN'
+    bl_space_type = 'VIEW_3D'
+    bl_region_type= 'UI'
+    bl_category= 'StyleGAN'
+
+    def draw(self, context):
+        layout = self.layout
+        scene = context.scene
+        props = scene.props
+        row = layout.row()
+        layout.prop(props, 'network')
+        layout.prop(props, 'seed')
+        
+        row = layout.row()
+        row.operator("stylegan.run")
+
+classes = (
+    MyPanel,
+    properties,
+    stylegan_OT_run
+)
+
+
+def register():
+    from bpy.utils import register_class
+    for cls in classes:
+        register_class(cls)
+
+    bpy.types.Scene.props = bpy.props.PointerProperty(type=properties)
+
+def unregister():
+    from bpy.utils import unregister_class
+    for cls in reversed(classes):
+        unregister_class(cls)
+    del bpy.types.Scene.props
+    
+if __name__ == '__main__':
+    register()
+
+#----------------------------------------------------------------------------
+
+#    generate_images(
+#        "C:/pkl/textures.pkl",
+#        [512514],
+#        1,
+#        "const"
+#    )
+
+#----------------------------------------------------------------------------
