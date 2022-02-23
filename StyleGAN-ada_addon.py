b04827f4c3aae0a9202ebd80fa8c480f288378db
marianobasti <31198560+marianobasti@users.noreply.github.com>
2022-02-23 10:39:52 -0300

    Update StyleGAN-ada_addon.py

--- StyleGAN-ada_addon.py
+++ StyleGAN-ada_addon.py
@@ -1,6 +1,6 @@
 bl_info = {
     "name": "StyleGAN for Blender",
-    "version": (0, 2),
+    "version": (0, 3),
     "blender": (3, 0, 0),
     "location": "View3D > Panel > StyleGAN",
     "description": "Inference StyleGAN trained models to generate textures",
@@ -14,16 +14,57 @@ from bpy import context, data, ops
 import os
 import re
 from typing import List, Optional
-
-import click
-import dnnlib
-import numpy as np
-import PIL.Image
-import torch
-import legacy
 import glob
+import subprocess
+import sys
 
-import cv2
+# Check if dependencies are installed, if not, install them.
+def check_install_dependencies():
+    try:
+        import click
+        import dnnlib
+        import numpy as np
+        import PIL.Image
+        import torch
+        import legacy
+        import cv2
+    except ImportError:
+        import subprocess
+        # Check OS and CUDA version
+        if sys.platform == 'linux' or sys.platform == 'linux2':
+            if subprocess.call('which nvidia-smi', shell=True) == 0:
+                if subprocess.call('nvidia-smi -q | grep -i "Driver Version" | grep -Eo "[0-9]{1,2}\.[0-9]{1,2}"', shell=True) == 0:
+                    subprocess.call('sudo apt-get install -y --no-install-recommends nvidia-cuda-toolkit', shell=True)
+                else:
+                    print('CUDA version is too low. Please update to CUDA version 10.0 or higher.')
+                    return
+            else:
+                print('No CUDA GPU found. Please install CUDA 10.0 or higher.')
+                return
+        elif sys.platform == 'win32':
+            # Check CUDA version
+            if subprocess.call('where nvcc', shell=True) == 0:
+                if subprocess.call('nvcc --version', shell=True) == 0:
+                    subprocess.call([sys.executable, "-m", "pip", "install", "torch==1.10.1+cu113 torchvision==0.11.2+cu113 torchaudio===0.10.1+cu113 -f https://download.pytorch.org/whl/cu113/torch_stable.html"])
+                else:
+                    print('CUDA version is too low. Please update to CUDA version 10.0 or higher.')
+            else:
+                print('No CUDA GPU found. Please install CUDA 10.0 or higher.')
+
+            subprocess.call([sys.executable, "-m", "pip", "install", "click", "dnnlib", "numpy", "PIL", "opencv-python"])
+        else:
+            print('Unknown OS. Whatever. Check line 55 of StyleGAN-ada_addon.py')
+            return
+        import click
+        import dnnlib
+        import numpy as np
+        import PIL.Image
+        import torch
+        import legacy
+        import cv2
+        
+
+
 from basicsr.archs.rrdbnet_arch import RRDBNet
 from realesrgan import RealESRGANer
 
@@ -66,15 +107,7 @@ def num_range(s: str) -> List[int]:
 #----------------------------------------------------------------------------
 def generate_images(network_pkl, seeds, truncation_psi, noise_mode, vector, param, sr, upscaleFactor):
     print('Loading networks from "%s"...' % network_pkl)
-    #device = torch.device('cuda')
-    #with dnnlib.util.open_url(network_pkl) as f:
-    #    G = legacy.load_network_pkl(f)['G_ema'].to(device) # type: ignore
-        
-    #G = torch.load(network_pkl)
-
-    if seeds is None:
-        ctx.fail('--seeds option is required when not using --projected-w')
-
+    
     # Labels.
     label = torch.zeros([1, G.c_dim], device=device)
     
@@ -87,9 +120,11 @@ def generate_images(network_pkl, seeds, truncation_psi, noise_mode, vector, para
         img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
         im = PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB')
 
-
+    mat = bpy.context.view_layer.objects.active.active_material
+    image_node = mat.node_tree.nodes["Image Texture"]
+    
     if sr == True:
-        #RealESRGAN
+        #RealESRGAN upscale
         torch.cuda.empty_cache()
         model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
 
@@ -110,28 +145,56 @@ def generate_images(network_pkl, seeds, truncation_psi, noise_mode, vector, para
             output, _ = upsampler.enhance(img, outscale=4)
         except Exception as error:
             print('Error', error)
-            print('If you encounter CUDA out of memory, try to set --tile with a smaller number.')     
-        mat = bpy.context.view_layer.objects.active.active_material
-        image_node = mat.node_tree.nodes["Image Texture"]
+            print('If you encounter CUDA out of memory, try to set --tile with a smaller number.')
         for img in bpy.data.images:
             bpy.data.images.remove(img)
         output = PIL.Image.fromarray(output, 'RGB')
         output =  pil_to_image(output)
         image_node.image = output
     else:
-        mat = bpy.context.view_layer.objects.active.active_material
-        image_node = mat.node_tree.nodes["Image Texture"]
         for img in bpy.data.images:
             bpy.data.images.remove(img)
         output = pil_to_image(im)
         image_node.image = output
-        torch.cuda.empty_cache()
+        
+    torch.cuda.empty_cache()
 
 
 def updateNdarray(seed):
     global ndarray
     ndarray = np.random.RandomState(seed).randn(1, G.z_dim)
 #----------------------------------------------------------------------------
+
+#Preferences
+class StyleGANforBlender(bpy.types.AddonPreferences):
+    bl_idname = __name__
+
+    cudaversion : EnumProperty(
+        items=[("102","10.2","10.2"),("113","11.3","11.3")],
+        name="CUDA version",
+        description="CUDA version for pytorch installation. It will take a while"
+    )
+
+    def draw(self, context):
+        layout = self.layout
+        row = layout.row()
+        layout.prop(self, 'cudaversion')
+        row.operator("stylegan.installdeps")
+
+# Dependency installation panel
+class INSTALLDEPS(Panel):
+    bl_label = "Install Dependencies"
+    bl_idname = "INSTALLDEPS"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "StyleGAN"
+    bl_context = "objectmode"
+
+    def draw(self, context):
+        layout = self.layout
+        row = layout.row()
+        row.operator("stylegan.installdeps")
+
 #Main Panel
 class PANEL_PT_StyleGAN2(Panel):
     bl_label = 'StyleGAN'
@@ -206,13 +269,26 @@ class stylegan_OT_run(bpy.types.Operator):
         generate_images(props.network, [props.seed],1,'const', props.vector, props.param, props.SuperResolution, props.SuperResolutionFactor)
         return{'FINISHED'}
 
+# Install dependencies operator
+class stylegan_OT_installDependencies(bpy.types.Operator):
+    bl_label = "Install Dependencies"
+    bl_idname = "stylegan.installdeps"
+    bl_parent_id = 'INSTALLDEPS'
+    bl_space_type = 'VIEW_3D'
+    bl_region_type= 'UI'
+    bl_category= 'StyleGAN'
+    
+    def execute(self,context):
+        check_install_dependencies()
+        return{'FINISHED'}
+
 #Render animation with animated parameters
 class stylegan_OT_renderanim(bpy.types.Operator):
     bl_label = "Render Animation"
     bl_idname = "stylegan.renderanim"
     
     def execute(self,context):
-        s=bpy.context.scene
+        
         s.render.resolution_x = 1080 # just for my example
         s.render.resolution_y = 1080
         props = s.props
@@ -234,10 +310,12 @@ class stylegan_OT_renderanim(bpy.types.Operator):
 
 classes = (
     PANEL_PT_StyleGAN2,
+    INSTALLDEPS,
     properties,
     stylegan_OT_run,
     stylegan_OT_loadNetwork,
-    stylegan_OT_renderanim
+    stylegan_OT_renderanim,
+    stylegan_OT_installDependencies
 )
 
 
